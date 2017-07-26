64a3a9f1fa0b0c58a2d10fd3f9a58dccb47d57b0
Christian Twilfer <c.twilfer@tec-networks.de>
2017-07-26 18:50:29 +0200

    Update deployazurestack.ps1

--- deployazurestack.ps1
+++ deployazurestack.ps1
@@ -52,6 +52,36 @@ Enable-WSManCredSSP -Role Client -DelegateComputer *
 # Change T-Shirt Size of the Virtula Machine
 # Change the T-Shirt Size within the Azure Portal to E16sv3
 
+cls
+#Login-AzureRmAccount
+
+# Variables
+$ResourceGroupName = "ResGroup"
+$VMName = "vmname"
+#$VMSize = "Standard_A3"
+$VMSize = "Standard_A1"
+$starttime = get-date -format hh:mm:ss
+Write-host "Starting resize at " $starttime
+
+# Shut down VM
+# VM must be shut down before size change to switch between A/D/DS/Dv2/G/GS/N
+
+Stop-AzureRmVm -name $VMName -ResourceGroupName $ResourceGroupName -StayProvisioned -Force
+
+# Resize VM 
+$vm = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName
+$vm.HardwareProfile.VirtualMachineSize = $VMSize
+Update-AzureRmVM -ResourceGroupName $ResourceGroupName -VM $vm
+
+$endtime = get-date -format hh:mm:ss
+$time = New-TimeSpan -Start $starttime -End $endtime
+write-host "Resize finished. Time to complete: " $time.minutes"minute(s) and "$time.seconds "seconds."
+
+# Start VM
+
+Start-AzureRmVm -name $VMName -ResourceGroupName $ResourceGroupName
+
+
 
 # Step 11:
 # Install POC
