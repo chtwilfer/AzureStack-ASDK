16f824376edf21b60f93f4ddd623affc4499a3d7
Christian Twilfer <christian.twilfer@outlook.de>
2017-10-19 11:31:32 +0200

    Update Deploy_Azure_Stack.ps1

--- Deploy_Azure_Stack.ps1
+++ Deploy_Azure_Stack.ps1
@@ -6,16 +6,15 @@
     The Steps:
     --------------------Script 1 -----------------------
     1. Deploy a ResourceGroup, Windows VM 2016 Datacenter, VNet and NSG
-    2. Expand the OS Disk to 256 GB and add 4 disks per 180 GB of each disk, set the 4 disk online
+    2. (Expand the OS Disk to 180 GB and) add 4 disks per 180 GB of each disk, set the 4 disk online
     3. disable IE Enhanced Security Configuration
-    4. Rename Admin Username
-    5. Install different Features
+    4. (Rename Admin Username)
+    5. (Install different Features)
     ---------------------Scripte 2 ---------------------
     6. integrate Networkshare and copy files and folders
-    7. Change manually IP-Settings
-    8. Start ASDK-Installer.ps1
-    9. Change file in folder
-    10. Start Installation
+    7. Start ASDK-Installer.ps1
+    8. Change file in folder
+    9. Start Installation
 
  .NOTES
   Version:          1.0
@@ -31,29 +30,41 @@
 # Step 6:
 # Integrate Networkshare "installshare" and copy files and folders
 net use x: \\asdkfiles.file.core.windows.net\installshare  /u:AZURE\asdkfiles 57vSHmJhLvBtZ6J1/WzguHp9gqbpSsAgxU63vxArX6G4Q93meIY0iDXXQTMhCI0GBQq3ukGd2cNXUhW5FzDtvA==
-Copy-Item x:\* C:\
 
-# Step 7:
-# change manually local Networksettings 
-# IP-Adresse: 172.16.0.4, Subnetmask: 255.255.255.0, Gateway: 172.16.0.1, DNS: 8.8.8.8
 
-# Step 8:
+# Step7:
 # Start asdk-installer.ps1
-# Install POC with ADFS [NOT AAD]
 cd C:\ASDK-Installer\
 .\asdk-installer.ps1
-# do not Reboot the Azure Machine.
+# Reboot the Azure Machine.
 
-# Step 9:
-# change file BareMetal.Tests.ps1 from folder "modified-if necessary" in "C:\CloudDeployment\Roles\PhysicalMachines\Tests"
 
-#Reboot the Azure Machine after Assistent
+# Step 8:
+# this Expands diff. files
+cd C:\CloudDeployment\Setup
+.\BootstrapAzureStackDeployment.ps1
 
-# Step 10: Start Installation with ADFS
+
+# then change file BareMetal.Tests.ps1 from Host-Folder (D:\) "modified-if necessary" in "C:\CloudDeployment\Roles\PhysicalMachines\Tests"
+
+
+# Step 9: Start Installation
+cd d:\ASDK-Installer
+# copy this folder to c:\
+# start AzureStack Installation
+cd c:\asdk-installer
+.\asdk-installer.ps1
+
+# alternative with ADFS
 cd C:\CloudDeployment\Setup
 .\InstallAzureStackPOC.ps1 -UseADFS -NATIPv4Subnet 172.16.0.0/24 -NATIPv4Address 172.16.0.5 -NATIPv4DefaultGateway 172.16.0.1 -Verbose
 
+# alternative with Azure Active Directory
+cd c:\CloudDeployment\Setup
+.\InstallAzureStackPOC.ps1 -InfraAzureDirectoryTenantName [yourdirectory].onmicrosoft.com -NATIPv4Subnet 172.16.0.0/24 -NATIPv4Address 172.16.0.5 -NATIPv4DefaultGateway 172.16.0.1 -Verbose
 
+
+<#
 ########################################if necessary ########################################
 
 # Enable CredSSP - if necessary - change and then rerun .\InstallAzureStackPOC.ps1 -Rerun -Verbose
@@ -68,9 +79,15 @@ Enable-WSManCredSSP -Role Client -DelegateComputer *
 New-VMSwitch -Name "NATSwitch" -SwitchType Internal -Verbose
 $NIC=Get-NetAdapter|Out-GridView -PassThru
 New-NetIPAddress -IPAddress 172.16.0.1 -PrefixLength 24 -InterfaceIndex $NIC.ifIndex
-New-NetNat -Name "NATSwitch" -InternalIPInterfaceAddressPrefix "172.16.0.0/24" –Verbose
+New-NetNat -Name "NATSwitch" -InternalIPInterfaceAddressPrefix "172.16.0.0/24" -Verbose
+
+
+
 
 
 # Rerun the Installation
 cd C:\CloudDeploment\Setup
 .\InstallAzureStackPOC.ps1 -Rerun -Verbose
+
+
+#>
