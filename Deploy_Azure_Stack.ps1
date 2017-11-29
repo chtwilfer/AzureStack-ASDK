4b5f3c251cc0b046ee272d16340cf2723836778a
Christian Twilfer <christian.twilfer@outlook.de>
2017-11-29 08:58:26 +0100

    Update Deploy_Azure_Stack.ps1

--- Deploy_Azure_Stack.ps1
+++ Deploy_Azure_Stack.ps1
@@ -11,10 +11,9 @@
     4. (Rename Admin Username)
     5. (Install different Features)
     ---------------------Scripte 2 ---------------------
-    6. integrate Networkshare and copy files and folders
+    6. Download ASDK
     7. Start ASDK-Installer.ps1
-    8. Change file in folder
-    9. Start Installation
+
 
  .NOTES
   Version:          1.0
@@ -28,29 +27,10 @@
 #>
 
 # Step 6:
-# Integrate Networkshare "installshare" and copy files and folders
-net use x: \\asdkfiles.file.core.windows.net\installshare  /u:AZURE\asdkfiles 57vSHmJhLvBtZ6J1/WzguHp9gqbpSsAgxU63vxArX6G4Q93meIY0iDXXQTMhCI0GBQq3ukGd2cNXUhW5FzDtvA==
+# Download ASDK from this Link: https://azure.microsoft.com/en-us/overview/azure-stack/development-kit/
 
+# Step 7: Start Installation
 
-# Step7:
-# Start asdk-installer.ps1
-cd C:\ASDK-Installer\
-.\asdk-installer.ps1
-# Reboot the Azure Machine.
-
-
-# Step 8:
-# this Expands diff. files
-cd C:\CloudDeployment\Setup
-.\BootstrapAzureStackDeployment.ps1
-
-
-# then change file BareMetal.Tests.ps1 from Host-Folder (D:\) "modified-if necessary" in "C:\CloudDeployment\Roles\PhysicalMachines\Tests"
-
-
-# Step 9: Start Installation
-cd d:\ASDK-Installer
-# copy this folder to c:\
 # start AzureStack Installation
 cd c:\asdk-installer
 .\asdk-installer.ps1
@@ -62,28 +42,6 @@ cd C:\CloudDeployment\Setup
 # alternative with Azure Active Directory
 cd c:\CloudDeployment\Setup
 .\InstallAzureStackPOC.ps1 -InfraAzureDirectoryTenantName [yourdirectory].onmicrosoft.com -NATIPv4Subnet 172.16.0.0/24 -NATIPv4Address 172.16.0.5 -NATIPv4DefaultGateway 172.16.0.1 -Verbose
-
-
-<#
-########################################if necessary ########################################
-
-# Enable CredSSP - if necessary - change and then rerun .\InstallAzureStackPOC.ps1 -Rerun -Verbose
-Enable-WSManCredSSP -Role Server
-Set-Item wsman:localhost\client\trustedhosts -Value *
-Enable-WSManCredSSP -Role Client -DelegateComputer *
-# Open the gpedit.msc console and navigate to Local Computer Policy > Computer Configuration > Administrative Templates > System > Credential Delegation.
-# Activate Allow Delegating Fresh Credentials with NTLM-only Server Authentication and add the value WSMAN/*. 
-
-
-# Change BGBNAT Switch for external Internet  [OPTIONAL]
-New-VMSwitch -Name "NATSwitch" -SwitchType Internal -Verbose
-$NIC=Get-NetAdapter|Out-GridView -PassThru
-New-NetIPAddress -IPAddress 172.16.0.1 -PrefixLength 24 -InterfaceIndex $NIC.ifIndex
-New-NetNat -Name "NATSwitch" -InternalIPInterfaceAddressPrefix "172.16.0.0/24" -Verbose
-
-
-
-
 
 # Rerun the Installation
 cd C:\CloudDeploment\Setup
