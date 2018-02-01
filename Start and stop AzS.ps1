fec46b5695a398d48dbad777c06571a308ad5b72
Christian Twilfer <christian.twilfer@outlook.de>
2018-02-01 09:07:10 +0100

    Create Start and stop AzS.ps1

---
+++ Start and stop AzS.ps1
@@ -0,0 +1,28 @@
+## How to shutdown and start an Azure Stack system
+
+# Christian Twilfer
+# 01.02.2018
+
+
+# Connect to the Privileged Endpoint via PowerShell Remoting
+
+$Pep = 'azs-ercs01'
+$cred = Get-Credential -UserName 'azurestack\cloudadmin' -Message 'Enter CloudAdmin Password'
+enter-pssession -computer $Pep -ConfigurationName PrivilegedEndpoint -Credential $cred
+
+# to Stop AzureStack
+Stop-AzureStack
+
+# to check Status
+Get-ActionStatus Stop-AzureStack
+
+# to Start AzureStack
+Start-AzureStack
+
+# to check Status
+Get-ActionStatus Start-AzureStack
+
+# to Test AzureStack
+Test-AzureStack
+
+End-pssession
